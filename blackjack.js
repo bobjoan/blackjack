780cf368f083125b21a43779d67f32c44bb2c2ad
bobjoan <bobjoan966@gmail.com>
2024-03-29 20:59:54 +0100

    added textContent

--- blackjack.js
+++ blackjack.js
@@ -7,6 +7,7 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 
 function startGame() {
+  sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
