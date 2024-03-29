09c4ad21c95245e451ee714f51b4db12be8bbd94
bobjoan <bobjoan966@gmail.com>
2024-03-29 21:47:18 +0100

    added new function

--- blackjack.js
+++ blackjack.js
@@ -5,9 +5,14 @@ let message = "";
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("card-el");
+
+startButton.addEventListener("click", startGame);
 
 function startGame() {
   sumEl.textContent = "Sum: " + sum;
+  cardEl.textContent = "Cards: " + firstCard + " " + secondCard;
+
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -17,4 +22,10 @@ function startGame() {
   }
 
   messageEl.textContent = message;
+}
+
+newCardButton.addEventListener("click", newCard);
+
+function newCard() {
+  alert = "Drawing a new card from the deck!";
 }
