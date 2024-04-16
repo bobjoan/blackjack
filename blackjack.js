d627ef1a9148f93c12553e7d0f6a7d5f69181ee1
bobjoan <bobjoan966@gmail.com>
2024-04-16 21:15:53 +0100

    updated game

--- blackjack.js
+++ blackjack.js
@@ -1,19 +1,41 @@
-let firstCard = 10;
-let secondCard = 4;
-let sum = firstCard + secondCard;
-let cards = [firstCard, secondCard]; // array of items
-
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = true;
 let message = "";
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardEl = document.getElementById("card-el");
 
+let player = {
+  name: "Joan",
+  win: 200,
+};
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.win;
 startButton.addEventListener("click", renderGame); // listener for start game
 newCardButton.addEventListener("click", newCard); // listener for new card selector
 
 function startGame() {
+  isAlive = true;
+  let firstCard = rollDice();
+  let secondCard = rollDice();
+  sum = firstCard + secondCard;
+  cards = [firstCard, secondCard]; // array of items
   renderGame();
+}
+
+function rollDice() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1; //1 - 13
+
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  }
+  return randomNumber;
 }
 
 function renderGame() {
@@ -30,14 +52,19 @@ function renderGame() {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
     message = "You've got Blackjack!";
+    hasBlackJack = true;
   } else {
     message = "You're out of the game!";
+    isAlive = false;
   }
+  messageEl.textContent = message;
 }
 
 function newCard() {
-  let card = 5;
-  sum += card;
-  cards.push(card);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = rollDice();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
